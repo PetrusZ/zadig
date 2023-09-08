283b5c81ebdf6d22f388ce3895364b3439f2911d
Patrick Zhao <zhaoyu@koderover.com>
2023-09-08 10:37:55 +0800

    fix bugs
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>

--- pkg/microservice/aslan/core/common/repository/mongodb/configuration_management.go
+++ pkg/microservice/aslan/core/common/repository/mongodb/configuration_management.go
@@ -26,7 +26,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/koderover/zadig/pkg/microservice/aslan/config"
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/models"
@@ -53,14 +52,7 @@ func (c *ConfigurationManagementColl) GetCollectionName() string {
 }
 
 func (c *ConfigurationManagementColl) EnsureIndex(ctx context.Context) error {
-	mod := mongo.IndexModel{
-		Keys: bson.D{
-			bson.E{Key: "server_address", Value: 1},
-		},
-		Options: options.Index().SetUnique(true),
-	}
-
-	_, err := c.Indexes().CreateOne(ctx, mod)
+	_, err := c.Indexes().DropOne(ctx, "server_address")
 	return err
 }
 
